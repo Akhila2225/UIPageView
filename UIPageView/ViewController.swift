975baec748353d16c8819bc583a15fb80ee2d123
Akhila Chatharaju <akhila2225@gmail.com>
2018-12-22 10:38:26 -0600

    Initial Commit

---
+++ UIPageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPageView
+//
+//  Created by Akhila Chatharaju on 12/22/18.
+//  Copyright © 2018 Akhila Chatharaju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
