ae612c5064a3ff8a6f1aee9608cb9e47316b1279
Akhila2225 <akhila2225@gmail.com>
2018-12-26 14:25:02 -0600

    first commit

---
+++ UIPageView/ViewController3.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController3.swift
+//  UIPageView
+//
+//  Created by Akhila Chatharaju on 12/22/18.
+//  Copyright © 2018 Akhila Chatharaju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
